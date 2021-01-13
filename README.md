446baeec1055ae16157d289bd341688d6ed9553b
dymafr <erwan1309@hotmail.com>
2021-01-13 17:35:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# exemple-spinner-progression
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/exemple-spinner-progression)
---
+++ src/app/exemple.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { Subscription, timer } from "rxjs";
+import { takeWhile } from "rxjs/operators";
+
+@Component({
+  selector: "app-exemple-component",
+  templateUrl: "exemple.html",
+  styleUrls: ["exemple.scss"]
+})
+export class ExempleComponent {
+  color = "primary";
+  progression = 0;
+  sub: Subscription;
+
+  ngOnInit() {
+    this.createCounter();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  createCounter() {
+    this.sub = timer(0, 100)
+      .pipe(takeWhile(x => x <= 100))
+      .subscribe(x => {
+        this.progression = x;
+      });
+  }
+
+  reset() {
+    this.color = "primary";
+    this.sub.unsubscribe();
+    this.progression = 0;
+    this.createCounter();
+  }
+}
